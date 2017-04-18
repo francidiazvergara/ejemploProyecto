9691532a956b9bd4dcd77784f1a440eedc741ed3
francidiazvergara <francidiazvergara@gmail.com>
2017-04-18 16:38:00 -0300

    agregando actividad

---
+++ proyecto2_martes/app/src/main/java/com/example/pannxxoo/proyecto2_martes/prueba_2.java
@@ -0,0 +1,13 @@
+package com.example.pannxxoo.proyecto2_martes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class prueba_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prueba_2);
+    }
+}
